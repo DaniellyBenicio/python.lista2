cebeec230f57eb367fda41ed3daf43646b1c7f57
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-04-24 16:41:53 -0300

    Update q17.py

--- q17.py
+++ q17.py
@@ -1,17 +1,22 @@
 '''Peça para o usuário digitar 3 nomes e crie uma tupla com esses nomes. 
 Em seguida, verifique quantas vezes o nome 'Maria' aparece na tupla.'''
 
-nome1 = input('Digite o 1º nome: ')
-nome2 = input('Digite o 2º nome: ')
-nome3 = input('Digite o 3º nome: ')
+nome = []
 
-tup = (nome1, nome2, nome3)
+for i in range(1,4):
+    n = input(f'Informe o {i}º nome: ')
+    nome.append(n)
 
+tup = tuple(nome)
 qnt = 0
 
 for i in range(len(tup)):
     if tup[i].lower() == 'maria':
         qnt = qnt + 1
-print(f'O nome Maria está presente na tupla! Aparece {qnt} vezes!')
+print(f'O nome Maria aparece {qnt} vezes!')
 
-
+'''
+for i in tup:
+    if i.lower() == 'maria':
+    qnt = qnt + 1
+'''
